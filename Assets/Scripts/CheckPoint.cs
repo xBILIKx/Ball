51b82eea1c501f420d9141da2e8dfef2abced7af
xBILIKx <xBILIKx@users.noreply.github.com>
2020-07-10 21:33:17 +0300

    Final

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CheckPoint : MonoBehaviour
 {
     public UnityEvent setCheckPoint = new UnityEvent();
+    public GameObject checkPointText;
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             setCheckPoint.Invoke();
-            Destroy(gameObject);
+            StartCoroutine(CheckPointText());
         }
+    }
+
+    IEnumerator CheckPointText()
+    {
+        checkPointText.SetActive(true);
+        yield return new WaitForSeconds(3);
+        checkPointText.SetActive(false);
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/FinishController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishController : MonoBehaviour
+{
+    public GameObject panel;
+    private void OnTriggerEnter(Collider other)
+    {
+        Time.timeScale = 0f;
+        panel.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
